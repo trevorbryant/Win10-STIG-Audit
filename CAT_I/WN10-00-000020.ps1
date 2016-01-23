752a7bfc91fb7c24bcd8c99f710fc8a8897888d7
Trevor Bryant <trevorbryant@gmail.com>
2016-01-23 11:53:16 -0500

    reorganized

--- CAT_I/WN10-00-000020.ps1
+++ CAT_I/WN10-00-000020.ps1
@@ -1,15 +1,17 @@
 ﻿## Windows 10 STIG Compliance Audit
+## Version 1, Release 1
 ## Created by Trevor Bryant
-## 
+## 01/19/2016
 
-$GroupID = "V-63327"
-$GroupTitle = "WN10-00-000015"
-$RuleID = "SV-77817r1_rule"
-$Severity = "CAT II"
-$RuleVersionSTIGID = "WN10-00-000015"
-$RuleTitle = "System firmware or system controllers must have administrator accounts/passwords configured."
+$GroupID = "V-63331"
+$GroupTitle = "WN10-00-000020"
+$RuleID = "SV-77821r1_rule"
+$Severity = "CAT I"
+$RuleVersionSTIGID = "WN10-00-000020"
+$RuleTitle = "The system must not use removable media as the boot loader."
+$CCI = "CCI-000366"
 
-$OSver = (gwmi Win32_OperatingSystem).Caption
+$Configuration = ""
 $Audit = New-Object -TypeName System.Object
 $Audit | Add-Member -MemberType NoteProperty -Name GroupID -Value $GroupID
 $Audit | Add-Member -MemberType NoteProperty -Name GroupTitle -Value $GroupTitle
@@ -17,5 +19,6 @@ $Audit | Add-Member -MemberType NoteProperty -Name RuleID -Value $RuleID
 $Audit | Add-Member -MemberType NoteProperty -Name Severity -Value $Severity
 $Audit | Add-Member -MemberType NoteProperty -Name RuleVersionSTIGID -Value $RuleVersionSTIGID
 $Audit | Add-Member -MemberType NoteProperty -Name RuleTitle -Value $RuleTitle
-$Audit | Add-Member -MemberType NoteProperty -Name Configuration -Value $OSver
+$Audit | Add-Member -MemberType NoteProperty -Name CCI -Value $CCI
+$Audit | Add-Member -MemberType NoteProperty -Name Configuration -Value $Configuration
 $Audit
