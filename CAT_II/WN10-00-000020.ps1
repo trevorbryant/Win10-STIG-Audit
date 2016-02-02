da7eaaa474b64e012f6392bfbe8d59812d991047
Trevor Bryant <trevorbryant@gmail.com>
2016-02-01 19:02:09 -0500

    Moved WN10-00-000020.ps1 to correct directory

--- CAT_II/WN10-00-000020.ps1
+++ CAT_II/WN10-00-000020.ps1
@@ -1,7 +1,5 @@
 ﻿## Windows 10 STIG Compliance Audit
-## Version 1, Release 1
 ## Created by Trevor Bryant
-## 01/19/2016
 
 $GroupID = "V-63331"
 $GroupTitle = "WN10-00-000020"
@@ -11,7 +9,7 @@ $RuleVersionSTIGID = "WN10-00-000020"
 $RuleTitle = "The system must not use removable media as the boot loader."
 $CCI = "CCI-000366"
 
-$Configuration = ""
+$Configuration = "How to: Bootloader"
 $Audit = New-Object -TypeName System.Object
 $Audit | Add-Member -MemberType NoteProperty -Name GroupID -Value $GroupID
 $Audit | Add-Member -MemberType NoteProperty -Name GroupTitle -Value $GroupTitle
